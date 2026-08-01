235476a4ab7a54159ff9e4bc1b5f027dfa3bc019
Aarush Sharma <56772071+AarushDesigner@users.noreply.github.com>
2026-08-01 15:24:22 +0530

    Update firebase config.js

--- assets/js/firebase config.js
+++ assets/js/firebase config.js
@@ -1,16 +1,18 @@
 // assets/js/firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBZE-fuaL1auRnoXG8IOS4g7-xWnUi9nbU",
-  authDomain: "aarush-designer.firebaseapp.com",
-  projectId: "aarush-designer",
-  storageBucket: "aarush-designer.firebasestorage.app",
-  messagingSenderId: "783410104663",
-  appId: "1:783410104663:web:7c23c079d07eb2dec89612",
-  measurementId: "G-ETGGXBRPRL"
+    apiKey: "AIzaSyBZE-fuaL1auRnoXG8IOS4g7-xWnUi9nbU",
+    authDomain: "aarush-designer.firebaseapp.com",
+    projectId: "aarush-designer",
+    storageBucket: "aarush-designer.firebasestorage.app",
+    messagingSenderId: "783410104663",
+    appId: "1:783410104663:web:7c23c079d07eb2dec89612",
+    measurementId: "G-ETGGXBRPRL"
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 export const db = getFirestore(app);
